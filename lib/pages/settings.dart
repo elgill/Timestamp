86910658041d970209bf6972cfc501034c363ece
Chris Gillin <cgillin2@gmail.com>
2023-09-02 21:49:15 -0400

    Dynamic version number

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class SettingsScreen extends StatelessWidget {
+final packageInfoProvider = FutureProvider<PackageInfo>((ref) async {
+  return await PackageInfo.fromPlatform();
+});
+
+class SettingsScreen extends ConsumerWidget {
   const SettingsScreen({super.key});
 
   Future<void> _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
     } else {
       throw 'Could not launch $url';
     }
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final packageInfoAsyncValue = ref.watch(packageInfoProvider);
+
     return Scaffold(
       appBar: AppBar(title: const Text("Settings")),
       body: ListView(
@@ -24,23 +32,20 @@ class SettingsScreen extends StatelessWidget {
               _launchURL('https://gillin.dev/privacy');
             },
           ),
-/*          ListTile(
-            title: const Text('Terms of Service'),
-            onTap: () {
-              _launchURL('https://gillin.dev');
-            },
-          ),*/
           ListTile(
             title: const Text('Feedback & Support'),
             onTap: () {
               _launchURL('https://gillin.dev/#contact');
             },
           ),
-          const ListTile(
-            title: Text('App Version'),
-            subtitle: Text('1.0.0'), // replace with dynamic app version
-          ),
-          // Any other settings can be added here
+          ListTile(
+            title: const Text('Version'),
+            subtitle: packageInfoAsyncValue.when(
+              data: (packageInfo) => Text(packageInfo.version),
+              loading: () => const Text('Fetching version...'),
+              error: (err, stack) => const Text('Error fetching version'),
+            ),
+          )
         ],
       ),
     );
