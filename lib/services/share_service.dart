24af65a9fea583e6976699b1fcea7702cd9b5692
Chris Gillin <cgillin2@gmail.com>
2025-04-14 21:08:41 -0400

    upgrade share dependency

--- lib/services/share_service.dart
+++ lib/services/share_service.dart
@@ -1,25 +1,7 @@
-import 'package:path_provider/path_provider.dart';
-import 'package:share/share.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:timestamp/services/export_service.dart';
 import 'package:timestamp/models/event.dart';
 import 'package:timestamp/enums/time_format.dart';
-
-import 'dart:io';
-
-Future<void> exportAndShareEventsCsv(List<Event> events) async {
-  String content;
-  String fileName;
-
-  content = convertEventsToCsv(events);
-  fileName = 'events.csv';
-
-  final directory = await getApplicationDocumentsDirectory();
-  final File file = File('${directory.path}/$fileName');
-  await file.writeAsString(content);
-
-  Share.shareFiles([file.path], text: 'Here are the exported events.');
-}
-
 
 Future<void> exportAndShareEventsPlainText(List<Event> events, TimeFormat timeFormat) async {
   String plainText = convertEventsToPlainText(events, timeFormat);
